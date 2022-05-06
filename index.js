b5b08b0c379d0982c69c549212ae155f79edf84a
PranoySarker <pranoysarker01@gmail.com>
2022-05-06 18:37:25 +0600

    initial setup completed

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+var cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+//userDb
+//vsIZ5rhpD1h3oPl3
+
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.mbcbq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const collection = client.db("test").collection("devices");
+
+
+
+        console.log('connected to the database');
+    }
+    finally { }
+}
+
+run().catch(console.dir);
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     console.log('connected to the database');
+// perform actions on the collection object
+//   client.close();
+// });
+
+app.get('/', (req, res) => {
+    res.send('server is running');
+})
+
+app.listen(port, (req, res) => {
+    console.log('listening the port', port);
+})
