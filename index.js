d5de0a537b4d774537efca02a31d14231fbd5de9
PranoySarker <pranoysarker01@gmail.com>
2023-02-25 17:49:58 +0600

    issues to deploy solved

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    client.connect();
     const inventoryCollection = client
       .db('homeReserve')
       .collection('inventories');
@@ -82,74 +82,6 @@ run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('server is running');
-});
-
-async function run() {
-  try {
-    await client.connect();
-    const inventoryCollection = client
-      .db('homeReserve')
-      .collection('inventories');
-
-    //get all data
-    app.get('/inventory', async (req, res) => {
-      const query = req.body;
-      const cursor = inventoryCollection.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-
-    //get a single data
-    app.get('/inventory/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await inventoryCollection.findOne(query);
-      res.send(result);
-    });
-
-    //post data
-    app.post('/inventory', async (req, res) => {
-      const data = req.body;
-      const result = await inventoryCollection.insertOne(data);
-      res.send(result);
-    });
-
-    //update data
-    app.put('/inventory/:id', async (req, res) => {
-      const id = req.params.id;
-      const updatedQuantity = Number(req.body.quantity);
-      const query = { _id: ObjectId(id) };
-      const options = { upsert: true };
-      const updateDoc = {
-        $set: {
-          quantity: updatedQuantity,
-        },
-      };
-      const result = await inventoryCollection.updateOne(
-        query,
-        updateDoc,
-        options
-      );
-      res.send(result);
-    });
-
-    //delete a data
-    app.delete('/inventory/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await inventoryCollection.deleteOne(query);
-      res.send(result);
-    });
-
-    console.log('connected to the database');
-  } finally {
-  }
-}
-
-run().catch(console.dir);
-
-app.get('/pranoy', (req, res) => {
-  res.send('my name');
 });
 
 app.listen(port, (req, res) => {
